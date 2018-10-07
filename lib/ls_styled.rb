a179ca501ae52ae83e34b2ea7fd9a7f1f3731923
J2525 <blue.east1028@gmail.com>
2018-10-07 16:39:40 +0900

    Add -a -l -r option

--- lib/ls_styled.rb
+++ lib/ls_styled.rb
@@ -1,11 +1,39 @@
 require "ls_styled/version"
 
 module LsStyled
-  def self.lsstyled(path)
-    lists = Dir::entries(path)
+  def self.run(path, opts = {})
+    files = self.get_files(path, opts)
+    return self.l_option(files) if opts[:l]
+    puts files
+  end
+
+  def self.get_files(path, opts = {})
+    lists = opts[:r] ? Dir::entries(path).sort.reverse : Dir::entries(path).sort
+
+    return lists if opts[:a]
+
+    results = []
     lists.each do |list|
       next if list[0] == '.'
-      puts list
+      results << list
+    end
+    results
+  end
+
+  def self.l_option(files)
+    files.each do |file|
+      stat = File.stat(file)
+      results = [
+        stat.dev,
+        stat.mode,
+        stat.nlink,
+        Etc.getpwuid(stat.uid).name,
+        Etc.getgrgid(stat.gid).name,
+        stat.size,
+        stat.mtime,
+        file
+      ]
+      puts results.join(" ")
     end
   end
 end
